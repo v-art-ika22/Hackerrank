9c88faa8c7ab7b9abf4c334fff7693ce96e0cc59
v-art-ika22 <vartikaagnihotri9402@gmail.com>
2025-10-10 09:49:58 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();int rev=0;
+        while(n>0){
+            int d=n%10;
+            rev=rev*10+d;
+            n/=10;
+        }
+        System.out.print(rev);
+    }}
+    
+
